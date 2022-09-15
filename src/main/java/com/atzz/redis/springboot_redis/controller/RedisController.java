9bcc1001cea153969e32efeb66eb12d064f93cf9
NanGe <NanGe@168.com>
2022-09-15 11:14:06 +0800

    first commit

---
+++ src/main/java/com/atzz/redis/springboot_redis/controller/RedisController.java
@@ -0,0 +1,28 @@
+package com.atzz.redis.springboot_redis.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author shkstart
+ * @create 2022-09-13 16:41
+ */
+@RestController
+@RequestMapping("/redisTest")
+public class RedisController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @GetMapping
+    public String testRedis() {
+        redisTemplate.opsForValue().set("name","lucy");
+        String name = (String) redisTemplate.opsForValue().get("name");
+        return name;
+    }
+
+}
---
+++ src/test/java/com/atzz/redis/springboot_redis/SpringbootRedisApplicationTests.java
@@ -0,0 +1,17 @@
+package com.atzz.redis.springboot_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+    @Test
+    void test1() {
+
+    }
+
+}
